0be185da58984130c71646af239f9262b705f993
LHN <lhn000823@gmail.com>
2023-05-16 12:13:18 +0100

    Big data

--- src/components/echart/centerLeft/centerLeft2Chart/index.vue
+++ src/components/echart/centerLeft/centerLeft2Chart/index.vue
@@ -12,42 +12,42 @@ export default {
       cdata: [
         {
           // 名字需要与 “common/map/fujian.js” 地图数据文件里面定义的一一对应，不能是 “福州” 或者 “闽” 之类的缩写
-          name: '福州市',
+          name: '黄浦区',
           value: 10,
           elseData:{
             // 这里放置地图 tooltip 里想显示的数据
           }
         },
         {
-          name: '厦门市',
+          name: '徐汇区',
           value: 9,
         },
         {
-          name: '漳州市',
+          name: '长宁区',
           value: 8,
         },
         {
-          name: '泉州市',
+          name: '静安区',
           value: 7,
         },
         {
-          name: '三明市',
+          name: '普陀区',
           value: 6,
         },
         {
-          name: '莆田市',
+          name: '虹口区',
           value: 5,
         },
         {
-          name: '南平市',
+          name: '杨浦区',
           value: 4,
         },
         {
-          name: '龙岩市',
+          name: '闵行区',
           value: 3,
         },
         {
-          name: '宁德市',
+          name: '宝山区',
           value: 2,
         }
       ]
